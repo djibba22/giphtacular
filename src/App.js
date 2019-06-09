65d4489d6f05701630ca588615f4c146b3325b07
Djibba22 <johnwaynedinsmore@gmail.com>
2019-06-09 14:16:53 -0400

    updated form to a dumb component with props:wrench:

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import {React} from 'react';
+import React from "react";
 
 import './App.css';
 import Form from './components/Form/Form';
 import Button from './components/Button/Button';
 
 class App extends React.Component  {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      giphButtons:[]
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
   render(){
     return (
       <div className="App">
         <header className="App-header">
-          <Form/>
+          <Form 
+          change={this.handleChange}
+          submit={this.handleSubmit}
+          value={this.state.value}
+          />
         </header>
         <div class="container">
           <div class="row">
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,35 +1,17 @@
 import React from 'react';
 import './Form.css' 
 
-class Form extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {value: ''};
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-  
-    handleChange(event) {
-      this.setState({value: event.target.value});
-    }
-  
-    handleSubmit(event) {
-      alert('A name was submitted: ' + this.state.value);
-      event.preventDefault();
-    }
-  
-    render() {
+function Form (props) {
       return (
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={props.submit}>
           <label>
             Know Code?:
-            <input className="main-input" type="text" value={this.state.value} onChange={this.handleChange} />
+            <input className="main-input" type="text" value={props.value} onChange={props.change} />
           </label>
           <button class="btn btn-default" type="submit" value="Submit">ClickToNerd</button>
         </form>
       );
-    }
+  
   }
 
 export default Form;
